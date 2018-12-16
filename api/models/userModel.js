3bbd8357af982a973cf9fdacdc8ee640bce1e755
Joed Machado <joedmg58@gmail.com>
2018-12-16 11:30:17 -0500

    user model and user route created, added to app.js

---
+++ api/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    },
+    password: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    phoneNo: {type: String, required: true, unique: true, match: /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/},
+    carPlate: {type: String, required: true, unique: true, match: /([a-z]|[A-Z]|[0-9]){6}/}
+});
+
+module.exports = mongoose.model('User', userSchema);
