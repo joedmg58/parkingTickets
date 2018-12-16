2a072b6897906a5b46d8cf03cffdc20e4bee325f
Joed Machado <joedmg58@gmail.com>
2018-12-16 15:59:42 -0500

    ticket model created

--- api/models/ticketModel.js
+++ api/models/ticketModel.js
@@ -2,18 +2,11 @@ const mongoose = require('mongoose');
 
 const ticketSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: {
-        type: String, 
-        required: true, 
-        unique: true, 
-        match: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-    },
-    password: {type: String, required: true},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    phoneNo: {type: String, required: true, unique: true, match: /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/},
-    carPlate: {type: String, required: true, unique: true, match: /([a-z]|[A-Z]|[0-9]){6}/},
-    userImage: {type: String, required: false}
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    date: {},
+    location: {type: String, required: true},
+    amount: {type: Number, required: true},
+    ticketImage: {type: String, required: false}
 });
 
 module.exports = mongoose.model('Ticket', ticketSchema);
